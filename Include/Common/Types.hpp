e6b987a2f1b899be59d3a4afe62d67208f8dc0ea
Piotr Doan <doanpiotr@gmail.com>
2024-04-29 10:08:28 +0200

    Add standard types header

---
+++ Include/Common/Types.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+using i8 = signed char;
+using i16 = signed short;
+using i32 = signed int;
+using i64 = signed long long;
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using f32 = float;
+using f64 = double;
+
+using byte = u8;
+using word = u16;
+using dword = u32;
+using qword = u64;
+
+using size = u64;
+using ptrdiff = i64;
--- Include/Shared.hpp
+++ Include/Shared.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 #include "Common/Defines.hpp"
+#include "Common/Types.hpp"
 #include "Common/Debug.hpp"
