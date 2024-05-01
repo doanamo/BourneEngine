6e5be6996e1315cf19d97c8e7d340f8d77b80f2f
Piotr Doan <doanpiotr@gmail.com>
2024-05-02 00:21:40 +0200

    Log engine version

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,5 +6,10 @@ int main()
     Memory::Setup();
     Logger::Setup();
 
+    LOG("Engine version: {} ({}-{}-{})", Version::Readable,
+        Version::ChangeNumber, Version::BranchName, Version::CommitHash);
+    LOG("Commit date: {}", Version::CommitDate);
+    LOG("Build date: {}", Version::BuildDate);
+
     return 0;
 }
