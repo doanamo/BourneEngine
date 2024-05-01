2f88bd3e55c96157a3d613eb0813f51b5724e0ca
Piotr Doan <doanpiotr@gmail.com>
2024-05-02 01:09:52 +0200

    Add always/evaluate assert macros

--- Include/Common/Assert.hpp
+++ Include/Common/Assert.hpp
@@ -15,24 +15,28 @@ void SetAssertCallback(AssertCallback* callback);
     Assert
 */
 
+#define ASSERT_SIMPLE(expression) \
+    if(!(expression)) \
+    { \
+        OnAssertCallback(STRINGIFY(expression), __FILE__, __LINE__, nullptr); \
+        Debug::Abort(); \
+    }
+
+#define ASSERT_MESSAGE(expression, message) \
+    if(!(expression)) \
+    { \
+        OnAssertCallback(STRINGIFY(expression), __FILE__, __LINE__, message); \
+        Debug::Abort(); \
+    }
+
+#define ASSERT_DEDUCE(arg1, arg2, arg3, ...) arg3
+#define ASSERT_CHOOSER(...) EXPAND(ASSERT_DEDUCE(__VA_ARGS__, ASSERT_MESSAGE, ASSERT_SIMPLE))
+#define ASSERT_ALWAYS(...) EXPAND(ASSERT_CHOOSER(__VA_ARGS__)(__VA_ARGS__))
+
 #if !defined(CONFIG_RELEASE)
-    #define ASSERT_SIMPLE(expression) \
-        if(!(expression)) \
-        { \
-            OnAssertCallback(STRINGIFY(expression), __FILE__, __LINE__, nullptr); \
-            Debug::Abort(); \
-        }
-
-    #define ASSERT_MESSAGE(expression, message) \
-        if(!(expression)) \
-        { \
-            OnAssertCallback(STRINGIFY(expression), __FILE__, __LINE__, message); \
-            Debug::Abort(); \
-        }
-
-    #define ASSERT_DEDUCE(arg1, arg2, arg3, ...) arg3
-    #define ASSERT_CHOOSER(...) EXPAND(ASSERT_DEDUCE(__VA_ARGS__, ASSERT_MESSAGE, ASSERT_SIMPLE))
-    #define ASSERT(...) EXPAND(ASSERT_CHOOSER(__VA_ARGS__)(__VA_ARGS__))
+    #define ASSERT(...) ASSERT_ALWAYS(__VA_ARGS__)
+    #define ASSERT_EVALUATE(...) ASSERT(__VA_ARGS__)
 #else
     #define ASSERT(...) ((void)0)
+    #define ASSERT_EVALUATE(expression, ...) (void)(expression)
 #endif
