b73d8b7420861d0b9cebd5a43ce48e00c372fa9e
Piotr Doan <doanpiotr@gmail.com>
2024-05-06 20:14:01 +0200

    Simple commandline parsing

--- Include/Core/CommandLine.hpp
+++ Include/Core/CommandLine.hpp
@@ -5,9 +5,11 @@ class CommandLine final
 public:
     static CommandLine& Get();
 
-    bool Setup(int argc, char** argv);
-    bool Param(const char* name, const char** value = nullptr) const;
+    bool Setup(u32 argc, char** argv);
+    bool GetParam(const char* name, const char** value = nullptr) const;
+    const char* GetExecutable() const;
 
 private:
-
+    u32 m_argc = 0;
+    char** m_argv = nullptr;
 };
--- Source/Core/CommandLine.cpp
+++ Source/Core/CommandLine.cpp
@@ -7,15 +7,49 @@ CommandLine& CommandLine::Get()
     return instance;
 }
 
-bool CommandLine::Setup(int argc, char** argv)
+bool CommandLine::Setup(u32 argc, char** argv)
 {
     ASSERT(argc > 0);
     ASSERT(argv != nullptr);
 
+    m_argc = argc;
+    m_argv = argv;
     return true;
 }
 
-bool CommandLine::Param(const char* name, const char** value) const
+bool CommandLine::GetParam(const char* name, const char** value) const
 {
+    ASSERT(name != nullptr);
+
+    // Parse parameter in '-name value` format. Parameters can start
+    // with '-' or '--'. Does not support '-name=value' for simplicity,
+    // so parameter name must be separated from value with a space.
+    for(int i = 1; i < m_argc; ++i)
+    {
+        const char* arg = m_argv[i];
+        if(arg[0] != '-')
+        {
+            continue;
+        }
+
+        const int nameOffset = arg[1] == '-' ? 2 : 1;
+        const char* nameBegin = arg + nameOffset;
+        if(strcmp(nameBegin, name) == 0)
+        {
+            if(value && i + 1 < m_argc)
+            {
+                *value = m_argv[i + 1];
+            }
+
+            return true;
+        }
+    }
+
     return false;
+}
+
+const char* CommandLine::GetExecutable() const
+{
+    ASSERT(m_argv);
+    return m_argv[0];
 }
