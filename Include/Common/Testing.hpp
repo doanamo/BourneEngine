ae3cc4be00dd22ef700dd721c492afc629cdd5dd
Piotr Doan <doanpiotr@gmail.com>
2024-06-02 21:12:37 +0200

    Testing macros and result

--- Include/Common/Testing.hpp
+++ Include/Common/Testing.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+enum class TestResult
+{
+    Unknown,
+    Passed,
+    Failed,
+};
+
+#define TEST_TRUE(expression) \
+    if(!(expression)) \
+    { \
+        return TestResult::Failed; \
+    }
+
+#define TEST_FALSE(expression) \
+    if(expression) \
+    { \
+        return TestResult::Failed; \
+    }
