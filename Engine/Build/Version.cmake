08417fc563f839af71928dda882f6a97b09bd6e9
Piotr Doan <doanpiotr@gmail.com>
2024-06-19 21:13:49 +0200

    Minor comments

--- Engine/Build/Version.cmake
+++ Engine/Build/Version.cmake
@@ -1,4 +1,4 @@
-# Execute Git commands to retrieve version info from repository.
+# Execute Git commands to retrieve version info from repository
 execute_process(COMMAND "${GIT_EXECUTABLE}" rev-list --all --count
     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_CHANGE_NUMBER)
 
@@ -11,5 +11,5 @@ execute_process(COMMAND "${GIT_EXECUTABLE}" log -1 --format=%ci
 execute_process(COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE GIT_BRANCH_NAME)
 
-# Generate header file with version info.
+# Generate header file with version info
 configure_file(${INPUT_FILE} ${OUTPUT_FILE})
