53a2484a68238ed26097f177dedf03be55b91d15
Piotr Doan <doanpiotr@gmail.com>
2024-07-13 13:26:20 +0200

    Ad tests for memory allocation tracking

--- Tests/Memory/TestOperators.cpp
+++ Tests/Memory/TestOperators.cpp
@@ -3,12 +3,20 @@
 
 TestResult Memory::TestOperators()
 {
+    u64 allocationCount = DefaultAllocator::GetAllocationCount();
+    u64 allocatedBytes = DefaultAllocator::GetAllocatedUsableBytes();
+
     // Test operator new
     {
         u32* value = new u32(42);
         TEST_TRUE(value != nullptr);
         TEST_TRUE(*value == 42);
+
+        TEST_TRUE(DefaultAllocator::GetAllocationCount() == allocationCount + 1);
+        TEST_TRUE(DefaultAllocator::GetAllocatedUsableBytes() == allocatedBytes + sizeof(u32));
         delete value;
+        TEST_TRUE(DefaultAllocator::GetAllocationCount() == allocationCount);
+        TEST_TRUE(DefaultAllocator::GetAllocatedUsableBytes() == allocatedBytes);
     }
 
     // Test operator new array
@@ -19,7 +27,12 @@ TestResult Memory::TestOperators()
         TEST_TRUE(values[1] == 2);
         TEST_TRUE(values[2] == 3);
         TEST_TRUE(values[3] == 4);
+
+        TEST_TRUE(DefaultAllocator::GetAllocationCount() == allocationCount + 1);
+        TEST_TRUE(DefaultAllocator::GetAllocatedUsableBytes() == allocatedBytes + sizeof(u32) * 4);
         delete[] values;
+        TEST_TRUE(DefaultAllocator::GetAllocationCount() == allocationCount);
+        TEST_TRUE(DefaultAllocator::GetAllocatedUsableBytes() == allocatedBytes);
     }
 
     return TestResult::Success;
