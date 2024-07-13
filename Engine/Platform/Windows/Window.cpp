1edb755e525df8b24639a32ac5dd97c513d90754
Piotr Doan <doanpiotr@gmail.com>
2024-07-13 22:22:14 +0200

    Improve window class handling

--- Engine/Platform/Windows/Window.cpp
+++ Engine/Platform/Windows/Window.cpp
@@ -1,18 +1,52 @@
 #include "Shared.hpp"
 #include "Platform/Window.hpp"
 
-LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+class WindowClass
+{
+public:
+    WindowClass(WNDPROC WndProc)
+    {
+        WNDCLASSEX wc = {};
+        wc.cbSize = sizeof(WNDCLASSEX);
+        wc.lpfnWndProc = WndProc;
+        wc.lpszClassName = GetClassName();
+        wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+        ASSERT(RegisterClassEx(&wc) != 0);
+    }
+
+    ~WindowClass()
+    {
+        ASSERT(UnregisterClass(GetClassName(), nullptr) != 0);
+    }
+
+    const char* GetClassName() const
+    {
+        return "WindowClass";
+    }
+};
+
+static Window* GetWindowFromUserData(HWND hwnd)
 {
     Window* window = (Window*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+    ASSERT(window);
+    return window;
+}
 
+LRESULT CALLBACK Window::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
     switch(uMsg)
     {
     case WM_NCCREATE:
+        // Forward extra parameter from CreateWindow() and store it as user data
         SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)((CREATESTRUCT*)lParam)->lpCreateParams);
         break;
 
     case WM_DESTROY:
-        window->m_hwnd = nullptr;
+        {
+            Window* window = GetWindowFromUserData(hwnd);
+            ASSERT(window->m_hwnd == hwnd);
+            window->m_hwnd = nullptr;
+        }
         break;
     }
 
@@ -31,25 +65,8 @@ bool Window::Open(const char* title, u32 width, u32 height)
 {
     ASSERT(m_hwnd == nullptr);
 
-    static bool ClassRegistered = false;
-    const char ClassName[] = "Window";
-
-    if(!ClassRegistered)
-    {
-        WNDCLASSEX wc = {};
-        wc.cbSize = sizeof(WNDCLASSEX);
-        wc.lpfnWndProc = Window::WndProc;
-        wc.lpszClassName = &ClassName[0];
-        if(!RegisterClassEx(&wc))
-        {
-            LOG_ERROR("Failed to register window class");
-            return false;
-        }
-
-        ClassRegistered = true;
-    }
-
-    m_hwnd = CreateWindowEx(0, &ClassName[0], title, WS_OVERLAPPEDWINDOW,
+    static WindowClass windowClass(Window::WndProc);
+    m_hwnd = CreateWindowEx(0, windowClass.GetClassName(), title, WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, width, height, nullptr, nullptr, nullptr, this);
 
     if(m_hwnd == nullptr)
@@ -82,5 +99,5 @@ void Window::Close()
 
 bool Window::IsOpen() const
 {
-    return m_hwnd;
+    return m_hwnd != nullptr;
 }
