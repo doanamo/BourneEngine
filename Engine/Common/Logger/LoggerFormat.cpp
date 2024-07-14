a6d95b76b58c22dab572b4695b156f1a3d6cffb7
Piotr Doan <doanpiotr@gmail.com>
2024-07-14 12:28:04 +0200

    Change prefix for thread local variables

--- Engine/Common/Logger/LoggerFormat.cpp
+++ Engine/Common/Logger/LoggerFormat.cpp
@@ -2,7 +2,7 @@
 #include "LoggerFormat.hpp"
 #include <ctime>
 
-static thread_local char m_loggerFormatBuffer[
+static thread_local char t_loggerFormatBuffer[
     LoggerMessage::FormatBufferSize + 1024];
 
 static const char* GetLogSeverityName(LogSeverity severity)
@@ -55,7 +55,7 @@ static const char* ParseLogSourcePath(const char* source)
 
 const char* LoggerFormat::Format(const LoggerMessage& message)
 {
-    m_loggerFormatBuffer[0] = '\0';
+    t_loggerFormatBuffer[0] = '\0';
 
     std::time_t time = std::time(nullptr);
     std::tm* now = std::localtime(&time);
@@ -65,15 +65,15 @@ const char* LoggerFormat::Format(const LoggerMessage& message)
         "%Y-%m-%d %H:%M:%S %z", now) >= 0, "Failed to format time");
 
 #ifdef CONFIG_DEBUG
-    ASSERT_EVALUATE(snprintf(m_loggerFormatBuffer, ArraySize(m_loggerFormatBuffer),
+    ASSERT_EVALUATE(snprintf(t_loggerFormatBuffer, ArraySize(t_loggerFormatBuffer),
         "[%s][%-7s] %s {%s:%u}\n", timeBuffer, GetLogSeverityName(message.GetSeverity()),
         message.GetText(), ParseLogSourcePath(message.GetSource()), message.GetLine()) >= 0,
         "Failed to format epilogue");
 #else
-    ASSERT_EVALUATE(snprintf(m_loggerFormatBuffer, ArraySize(m_loggerFormatBuffer),
+    ASSERT_EVALUATE(snprintf(t_loggerFormatBuffer, ArraySize(t_loggerFormatBuffer),
         "[%s][%-7s] %s\n", timeBuffer, GetLogSeverityName(message.GetSeverity()),
         message.GetText()) >= 0, "Failed to format epilogue");
 #endif
 
-    return m_loggerFormatBuffer;
+    return t_loggerFormatBuffer;
 }
