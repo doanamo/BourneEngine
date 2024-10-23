dcbd76d8cd8d3fe34157e66584e1c1dec1c88aa8
Piotr Doan <doanpiotr@gmail.com>
2024-10-23 13:20:50 +0200

    Implement all new/delete operators

--- Engine/Memory/Operators.cpp
+++ Engine/Memory/Operators.cpp
@@ -22,4 +22,62 @@ void operator delete(void* allocation, std::align_val_t alignment) noexcept
     Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, static_cast<u32>(alignment));
 }
 
-// #todo: Override all known operators just in case. Optionally assert when unintended.
+void* operator new[](std::size_t size)
+{
+    return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
+}
+
+void* operator new[](std::size_t size, std::align_val_t alignment)
+{
+    return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
+}
+
+void operator delete[](void* allocation) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, alignof(std::max_align_t));
+}
+
+void operator delete[](void* allocation, std::align_val_t alignment) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, static_cast<u32>(alignment));
+}
+
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept
+{
+    return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
+}
+
+void* operator new(std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
+{
+    return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
+}
+
+void operator delete(void* allocation, const std::nothrow_t&) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, alignof(std::max_align_t));
+}
+
+void operator delete(void* allocation, std::align_val_t alignment, const std::nothrow_t&) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, static_cast<u32>(alignment));
+}
+
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
+{
+    return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
+}
+
+void* operator new[](std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
+{
+    return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
+}
+
+void operator delete[](void* allocation, const std::nothrow_t&) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, alignof(std::max_align_t));
+}
+
+void operator delete[](void* allocation, std::align_val_t alignment, const std::nothrow_t&) noexcept
+{
+    Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, static_cast<u32>(alignment));
+}
