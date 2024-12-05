ac3dcfc5ec500b423c73751dff241fd985a10696
Piotr Doan <doanpiotr@gmail.com>
2024-12-05 21:45:25 +0100

    Conditional Windows include

--- Engine/Platform/Defines.hpp
+++ Engine/Platform/Defines.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
-#undef Yield
-
-#include <wrl.h>
-using Microsoft::WRL::ComPtr;
+#ifdef PLATFORM_WINDOWS
+    #define WIN32_LEAN_AND_MEAN
+    #define NOMINMAX
+    #include <Windows.h>
+    #undef Yield
+#endif
