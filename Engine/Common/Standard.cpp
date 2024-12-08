5e6d0860f363b34a3a13bf92c0c784676f0bac54
Piotr Doan <doanpiotr@gmail.com>
2024-12-08 20:58:30 +0100

    Review Memmem() code

--- Engine/Common/Standard.cpp
+++ Engine/Common/Standard.cpp
@@ -1,24 +1,29 @@
 #include "Shared.hpp"
 #include "Standard.hpp"
 
-void* Memmem(const void* haystack, u64 haystackSize, const void* needle, u64 needleSize)
+const void* Memmem(const void* haystack, u64 haystackSize, const void* needle, u64 needleSize)
 {
-    if(needleSize == 0)
+    if(haystack == nullptr || haystackSize == 0)
         return nullptr;
 
-    const u64* iterator = (u64*)haystack;
+    if(needle == nullptr || needleSize == 0)
+        return nullptr;
+
+    const u64* iterator = static_cast<const u64*>(haystack);
     while(true)
     {
         const u64 sizeRemaining = haystackSize - (iterator - haystack);
         if(sizeRemaining < needleSize)
             break;
 
-        iterator = (u64*)std::memchr(iterator, *(u8*)needle, sizeRemaining - needleSize + 1);
-        if(!iterator)
+        iterator = static_cast<const u64*>(std::memchr(iterator,
+            *static_cast<const u8*>(needle), sizeRemaining - needleSize + 1));
+
+        if(iterator == nullptr)
             break;
 
-        if(!std::memcmp(iterator, needle, needleSize))
-            return (void*)iterator;
+        if(std::memcmp(iterator, needle, needleSize) == 0)
+            return iterator;
 
         iterator++;
     }
