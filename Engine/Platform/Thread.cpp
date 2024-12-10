b66e30abc1b14d099f6a2e7de1536c6029d799ad
Piotr Doan <doanpiotr@gmail.com>
2024-12-10 23:56:43 +0100

    Platform thread functions for Linux

--- Engine/Platform/Thread.cpp
+++ Engine/Platform/Thread.cpp
@@ -3,15 +3,33 @@
 
 void Platform::Sleep(u64 milliseconds)
 {
+#if defined(PLATFORM_WINDOWS)
     ::Sleep(milliseconds);
+#elif defined(PLATFORM_LINUX)
+    usleep(milliseconds * 1000);
+#else
+    #error Unknown platform!
+#endif
 }
 
 void Platform::SleepForever()
 {
+#if defined(PLATFORM_WINDOWS)
     ::Sleep(INFINITE);
+#elif defined(PLATFORM_LINUX)
+    pause();
+#else
+    #error Unknown platform!
+#endif
 }
 
 void Platform::Yield()
 {
+#if defined(PLATFORM_WINDOWS)
     ::SwitchToThread();
+#elif defined(PLATFORM_LINUX)
+    sched_yield();
+#else
+    #error Unknown platform!
+#endif
 }
