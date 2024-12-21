c546d104e4c0a011df2b41fdeda08f6cb0e10460
Piotr Doan <doanpiotr@gmail.com>
2024-12-22 00:06:15 +0100

    Fix failing memory tests due to over aggressive Clang optimizations

--- Tests/Memory/TestMemoryOperators.cpp
+++ Tests/Memory/TestMemoryOperators.cpp
@@ -7,9 +7,13 @@ TestResult Memory::TestOperators()
     LOG_INFO("Running Memory::TestOperators...");
     Memory::TestStats memoryStats;
 
+    // Note: Keyword volatile is used here to stop Clang from optimizing out
+    // the allocations, which we need here for unit testing purposes.
+    // See: https://godbolt.org/z/r65fsf77W
+
     // Test operator new
     {
-        u32* value = new u32(42);
+        volatile u32* value = new u32(42);
         TEST_TRUE(memoryStats.ValidateAllocations(1, sizeof(u32)));
         TEST_TRUE(value != nullptr);
         TEST_TRUE(*value == 42);
@@ -20,7 +24,7 @@ TestResult Memory::TestOperators()
 
     // Test operator new array
     {
-        u32* values = new u32[4]{ 1, 2, 3, 4 };
+        volatile u32* values = new u32[4]{ 1, 2, 3, 4 };
         TEST_TRUE(memoryStats.ValidateAllocations(1, sizeof(u32) * 4));
         TEST_TRUE(values != nullptr);
         TEST_TRUE(values[0] == 1);
