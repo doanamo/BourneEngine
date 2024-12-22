da55245742467b9e1d6cfad119cb8444268d50bb
Piotr Doan <doanpiotr@gmail.com>
2024-12-22 21:05:33 +0100

    Const arguments in memory operators

--- Engine/Memory/MemoryOperators.cpp
+++ Engine/Memory/MemoryOperators.cpp
@@ -5,42 +5,42 @@
 // Note: This source file should be built by executables only, not libraries.
 // It will be added automatically with setup_cmake_executable() in CMakeLists.
 
-void* operator new(std::size_t size)
+void* operator new(const std::size_t size)
 {
     return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
 }
 
-void* operator new[](std::size_t size)
+void* operator new[](const std::size_t size)
 {
     return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
 }
 
-void* operator new(std::size_t size, std::align_val_t alignment)
+void* operator new(const std::size_t size, std::align_val_t alignment)
 {
     return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
 }
 
-void* operator new[](std::size_t size, std::align_val_t alignment)
+void* operator new[](const std::size_t size, std::align_val_t alignment)
 {
     return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
 }
 
-void* operator new(std::size_t size, const std::nothrow_t&) noexcept
+void* operator new(const std::size_t size, const std::nothrow_t&) noexcept
 {
     return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
 }
 
-void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
+void* operator new[](const std::size_t size, const std::nothrow_t&) noexcept
 {
     return Memory::DefaultAllocator::Allocate(size, alignof(std::max_align_t));
 }
 
-void* operator new(std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
+void* operator new(const std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
 {
     return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
 }
 
-void* operator new[](std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
+void* operator new[](const std::size_t size, std::align_val_t alignment, const std::nothrow_t&) noexcept
 {
     return Memory::DefaultAllocator::Allocate(size, static_cast<u32>(alignment));
 }
@@ -65,22 +65,22 @@ void operator delete[](void* allocation, std::align_val_t alignment) noexcept
     Memory::DefaultAllocator::Deallocate(allocation, Memory::UnknownSize, static_cast<u32>(alignment));
 }
 
-void operator delete(void* allocation, std::size_t size) noexcept
+void operator delete(void* allocation, const std::size_t size) noexcept
 {
     Memory::DefaultAllocator::Deallocate(allocation, size, alignof(std::max_align_t));
 }
 
-void operator delete[](void* allocation, std::size_t size) noexcept
+void operator delete[](void* allocation, const std::size_t size) noexcept
 {
     Memory::DefaultAllocator::Deallocate(allocation, size, alignof(std::max_align_t));
 }
 
-void operator delete(void* allocation, std::size_t size, std::align_val_t alignment) noexcept
+void operator delete(void* allocation, const std::size_t size, std::align_val_t alignment) noexcept
 {
     Memory::DefaultAllocator::Deallocate(allocation, size, static_cast<u32>(alignment));
 }
 
-void operator delete[](void* allocation, std::size_t size, std::align_val_t alignment) noexcept
+void operator delete[](void* allocation, const std::size_t size, std::align_val_t alignment) noexcept
 {
     Memory::DefaultAllocator::Deallocate(allocation, size, static_cast<u32>(alignment));
 }
