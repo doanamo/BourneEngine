fd44f2419ab9e8dd55b2d9a8a3f57daf0ad7daf6
Piotr Doan <doanpiotr@gmail.com>
2025-01-27 03:13:27 +0100

    Generate build info into source files instead of headers

---
+++ Engine/Build/Info.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace BuildInfo
+{
+    extern const char* const ProjectName;
+    extern const char* const ProjectPath;
+    extern const char* const ChangeNumber;
+    extern const char* const BranchName;
+    extern const char* const CommitHash;
+    extern const char* const CommitDate;
+};
---
+++ Engine/Build/Version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Common/Types.hpp"
+
+namespace EngineVersion
+{
+    extern const char* const Readable;
+    extern const u32 Major;
+    extern const u32 Minor;
+    extern const u32 Patch;
+}
