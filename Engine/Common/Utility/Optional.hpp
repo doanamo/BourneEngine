e45614c9f683981f90c0deb22c4b1aa1b6fd471a
Piotr Doan <doanpiotr@gmail.com>
2025-03-22 18:53:32 +0100

    Optional type constructor improvements

--- Engine/Common/Utility/Optional.hpp
+++ Engine/Common/Utility/Optional.hpp
@@ -8,32 +8,57 @@ class Optional final
 
 public:
     Optional() = default;
-    Optional(const Type& value)
-        : m_value(value)
-        , m_hasValue(true)
-    {}
+    Optional(const Optional& other)
+        : m_value(other.m_value)
+        , m_hasValue(other.m_hasValue)
+    {
+    }
+
+    Optional(Optional&& other) noexcept
+        : m_value(std::move(other.m_value))
+        , m_hasValue(other.m_hasValue)
+    {
+        other.m_hasValue = false;
+    }
 
     explicit Optional(Type&& value)
         : m_value(std::move(value))
         , m_hasValue(true)
-    {}
-
-    Optional(const Optional& other)
     {
-        *this = other;
     }
 
-    Optional(Optional&& other) noexcept
+    Optional(const Type& value)
+        : m_value(value)
+        , m_hasValue(true)
     {
-        *this = std::move(other);
     }
 
-    Optional& operator=(const Optional& other) = default;
+    Optional& operator=(const Optional& other)
+    {
+        m_value = other.m_value;
+        m_hasValue = other.m_hasValue;
+        return *this;
+    }
+
     Optional& operator=(Optional&& other) noexcept
     {
         m_value = std::move(other.m_value);
         m_hasValue = other.m_hasValue;
         other.m_hasValue = false;
+        return *this;
+    }
+
+    Optional& operator=(Type&& value)
+    {
+        m_value = std::move(value);
+        m_hasValue = true;
+        return *this;
+    }
+
+    Optional& operator=(const Type& value) noexcept
+    {
+        m_value = value;
+        m_hasValue = true;
         return *this;
     }
 
