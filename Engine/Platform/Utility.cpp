515b892040a0aadc055b9bde04884faff66f78d2
Piotr Doan <doanpiotr@gmail.com>
2025-04-26 22:39:57 +0200

    Add WriteStringToFileIfDifferent() function

--- Engine/Platform/Utility.cpp
+++ Engine/Platform/Utility.cpp
@@ -1,12 +1,39 @@
 #include "Shared.hpp"
 #include "Common/Utility/Utility.hpp"
 
+bool CheckFileExists(const StringView& filePath)
+{
+    struct stat stats;
+    return stat(*filePath, &stats) == 0;
+}
+
+bool WriteStringToFileIfDifferent(const StringView& filePath, const StringView& contents)
+{
+    if(!CheckFileExists(filePath))
+    {
+        return WriteStringToFile(filePath, contents);
+    }
+
+    String existingContent;
+    if(!ReadStringFromFile(filePath, existingContent))
+    {
+        return false;
+    }
+
+    if(existingContent != contents)
+    {
+        return WriteStringToFile(filePath, contents);
+    }
+
+    return true;
+}
+
 bool WriteStringToFile(const StringView& filePath, const StringView& contents)
 {
-    FILE* file = fopen(*filePath, "w");
+    FILE* file = fopen(*filePath, "wb");
     if(!file)
     {
-        LOG_ERROR("Failed to open \"" STRING_VIEW_FORMAT "\" for writing", STRING_VIEW_VARG(filePath));
+        LOG_ERROR("Failed to open file for writing: " STRING_VIEW_FORMAT, STRING_VIEW_VARG(filePath));
         return false;
     }
 
@@ -16,5 +43,41 @@ bool WriteStringToFile(const StringView& filePath, const StringView& contents)
     };
 
     const u64 written = fwrite(contents.GetData(), contents.GetCharSize(), contents.GetLength(), file);
-    return written == contents.GetLength();
+    if(written != contents.GetLength())
+    {
+        LOG_ERROR("Failed to write file contents: " STRING_VIEW_FORMAT, STRING_VIEW_VARG(filePath));
+        return false;
+    }
+
+    return true;
+}
+
+bool ReadStringFromFile(const StringView& filePath, String& contents)
+{
+    FILE* file = fopen(*filePath, "rb");
+    if(!file)
+    {
+        LOG_ERROR("Failed to open file for reading: " STRING_VIEW_FORMAT, STRING_VIEW_VARG(filePath));
+        return false;
+    }
+
+    SCOPE_GUARD
+    {
+        fclose(file);
+    };
+
+    fseek(file, 0, SEEK_END);
+    const u64 size = ftell(file);
+
+    contents.Resize(size, '\0');
+    fseek(file, 0, SEEK_SET);
+
+    const u64 read = fread(contents.GetData(), 1, size, file);
+    if(read != size)
+    {
+        LOG_ERROR("Failed to read file contents: " STRING_VIEW_FORMAT, STRING_VIEW_VARG(filePath));
+        return false;
+    }
+
+    return true;
 }
--- Engine/Platform/Utility.hpp
+++ Engine/Platform/Utility.hpp
@@ -2,4 +2,7 @@
 
 #include "Common/Containers/StringView.hpp"
 
+bool CheckFileExists(const StringView& filePath);
+bool WriteStringToFileIfDifferent(const StringView& filePath, const StringView& contents);
 bool WriteStringToFile(const StringView& filePath, const StringView& contents);
+bool ReadStringFromFile(const StringView& filePath, String& contents);
