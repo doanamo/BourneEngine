2008e228fe1049d207bdb1783137ae4be1e38019
Piotr Doan <doanpiotr@gmail.com>
2025-07-06 12:35:20 +0200

    Define exit codes enumeration

--- Engine/Exit.hpp
+++ Engine/Exit.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+struct ExitCodes
+{
+    enum Type : u32
+    {
+        Success = 0,
+        SetupFailed = 1,
+        TestsFailed = 2,
+        MemoryLeak = 3,
+    };
+};
+
 // #fixme: Tests currently report leaked memory due to static Test::Registry arrays.
 // Expose delegate for subscribing into static exit so the array can be freed.
-
-// #todo: Define various exit codes via an enumeration.
