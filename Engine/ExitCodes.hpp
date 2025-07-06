20671715f5b8a45541a455be702251f0bcc73418
Piotr Doan <doanpiotr@gmail.com>
2025-07-06 20:38:00 +0200

    Fixed resolved todo

--- Engine/ExitCodes.hpp
+++ Engine/ExitCodes.hpp
@@ -10,6 +10,3 @@ enum class ExitCode : u32
     DiscoverTestsFailed,
     QueryTestsFailed,
 };
-
-// #fixme: Tests currently report leaked memory due to static Test::Registry arrays.
-// Expose delegate for subscribing into static exit so the array can be freed.
