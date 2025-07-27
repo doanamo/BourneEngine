35c67283951b52d38240d08a4462800804b76e32
Piotr Doan <doanpiotr@gmail.com>
2025-07-27 14:19:25 +0200

    Rename Memory.Allocators.Inline tests to Memory.InlineAllocator

--- Tests/Memory/TestInlineAllocator.cpp
+++ Tests/Memory/TestInlineAllocator.cpp
@@ -1,12 +1,12 @@
 #include "Shared.hpp"
 
-TEST_DEFINE("Memory.Allocators.Inline", "String")
+TEST_DEFINE("Memory.InlineAllocator", "String")
 {
     InlineString<8> string = "Hello!!";
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(0, 0));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "StringAppend")
+TEST_DEFINE("Memory.InlineAllocator", "StringAppend")
 {
     InlineString<12> string;
     string += "Hello ";
@@ -15,13 +15,13 @@ TEST_DEFINE("Memory.Allocators.Inline", "StringAppend")
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(1));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "Array")
+TEST_DEFINE("Memory.InlineAllocator", "Array")
 {
     InlineArray<u32, 2> array = { 4, 2 };
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(0, 0));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "ArrayAppend")
+TEST_DEFINE("Memory.InlineAllocator", "ArrayAppend")
 {
     InlineArray<u32, 2> array;
     array.Add(4);
@@ -30,28 +30,28 @@ TEST_DEFINE("Memory.Allocators.Inline", "ArrayAppend")
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(1));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "EmptyString")
+TEST_DEFINE("Memory.InlineAllocator", "EmptyString")
 {
     InlineString<16> string;
     TEST_TRUE(string.GetCapacity() == 15);
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(0, 0));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "EmptyHeapString")
+TEST_DEFINE("Memory.InlineAllocator", "EmptyHeapString")
 {
     HeapString string;
     TEST_TRUE(string.GetCapacity() == 0);
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(0, 0));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "EmptyArray")
+TEST_DEFINE("Memory.InlineAllocator", "EmptyArray")
 {
     InlineArray<u32, 16> array;
     TEST_TRUE(array.GetCapacity() == 16);
     TEST_TRUE(memoryGuard.ValidateTotalAllocations(0, 0));
 }
 
-TEST_DEFINE("Memory.Allocators.Inline", "EmptyHeapArray")
+TEST_DEFINE("Memory.InlineAllocator", "EmptyHeapArray")
 {
     HeapArray<u32> array;
     TEST_TRUE(array.GetCapacity() == 0);
